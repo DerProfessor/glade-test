712cba512709f872dffd4c2e3003140bde6b00d9
Daniel Hackbarth <da_ha@uni-bremen.de>
2019-12-18 03:33:09 +0100

    add aterm

---
+++ aterm-0.1.0.1/Setup.hs
@@ -0,0 +1,14 @@
+{- |
+Module      :  $Id: Setup.hs 13959 2010-08-31 22:15:26Z cprodescu $
+Description :  Cabal Setup file
+Copyright   :  (c) DFKI GmbH 2010
+License     :  GPLv2 or higher, see LICENSE.txt
+
+Maintainer  :  Christian.Maeder@dfki.de
+Stability   :  stable
+Portability :  portable
+
+a file for Cabal
+-}
+import Distribution.Simple
+main = defaultMain
